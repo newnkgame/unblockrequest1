4af73971a9375a34d3046702fc3c06edcd1f3c22
newnkgame <100308608+newnkgame@users.noreply.github.com>
2022-04-08 12:09:17 -0400

    Create js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+var stuff = '';
+var num = prompt("Enter Number", stuff);
+
+var front = 'https://ultra';
+var end = '.womginx.repl.co/service/hvtrs8%2F-wuw%2Cvkcvopicsqeargt%2Ccmm-uq%2F';
+var el = document.getElementById('ifrm');
+
+window.location.replace(front+num+end);
